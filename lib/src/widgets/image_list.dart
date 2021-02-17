69447252968780b745e1d2c57a207b0744605ce0
0xtheMachine <aaronvsummers@gmail.com>
2021-02-17 23:29:15 +0000

    refactoring

--- lib/src/widgets/image_list.dart
+++ lib/src/widgets/image_list.dart
@@ -9,34 +9,37 @@ class ImageList extends StatelessWidget {
     return ListView.builder(
         itemCount: images.length,
         itemBuilder: (context, int index) {
-          return Card(
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-            clipBehavior: Clip.antiAlias,
-            margin: EdgeInsets.fromLTRB(20, 20, 20, 10),
-            elevation: 10,
-            child: Column(
-              children: [
-                Container(
-                  child: Image(
-                    image: NetworkImage(images[index].url),
-                    errorBuilder: (BuildContext context, Object exception,
-                        StackTrace stackTrace) {
-                      return Image(
-                          image: NetworkImage(
-                              "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/600px-No_image_available.svg.png"));
-                    },
-                  ),
-                ),
-                Container(
-                  child: Padding(
-                    padding: EdgeInsets.all(15),
-                    child: Text('${images[index].name}'),
-                  ),
-                ),
-              ],
-            ),
-          );
+          return buildCharacter(images[index]);
         });
   }
+}
+
+Widget buildCharacter(ImageModel image) {
+  return Card(
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+    clipBehavior: Clip.antiAlias,
+    margin: EdgeInsets.fromLTRB(20, 20, 20, 10),
+    elevation: 10,
+    child: Column(
+      children: [
+        Container(
+          child: Image(
+            image: NetworkImage(image.url),
+            errorBuilder: (BuildContext context, Object exception,
+                StackTrace stackTrace) {
+              return Image(
+                  image: NetworkImage(
+                      "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/600px-No_image_available.svg.png"));
+            },
+          ),
+        ),
+        Container(
+          child: Padding(
+            padding: EdgeInsets.all(15),
+            child: Text('${image.name}'),
+          ),
+        ),
+      ],
+    ),
+  );
 }
